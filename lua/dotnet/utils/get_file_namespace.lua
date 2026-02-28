b83e98b3ecb1ecf0e9da0abbdea7619a39501cb1
KaBooMa <kaboomaz@gmail.com>
2026-02-28 00:48:25 -0600

    fix: Namespace not resolving properly

--- lua/dotnet/utils/get_file_namespace.lua
+++ lua/dotnet/utils/get_file_namespace.lua
@@ -5,20 +5,34 @@ return function(path)
 	-- Table with each part of the path (folders and file)
 	-- ex: ["this", "is", "folders", "class.cs"]
 	local path_parts = vim.split(path, delimiter)
+	vim.notify("path:" .. path)
 
 	-- Remove the file name from the path
 	-- ex: ["this", "is", "folders"]
 	table.remove(path_parts)
 
 	-- Find the .csproj to use as the "root" of the project
+	-- HACK: This entire while loop feels wrong. Need to find a better way to determine `project_file_name`
+	-- RootNamespace also could exist and not match the file.
 	local found_project_root = false
 	local namespace_parts = {}
-	while #path_parts > 0 and not found_project_root do
-		local path = table.concat(path_parts, delimiter)
-		local found = vim.fn.glob(path .. delimiter .. "*.csproj")
-		table.insert(namespace_parts, 1, table.remove(path_parts))
+	while not found_project_root do
+		local current_path = table.concat(path_parts, delimiter)
+		vim.notify("c: " .. current_path)
+		local glob_path = #path_parts > 0 and (current_path .. delimiter .. "*.csproj") or "*.csproj"
+		local found = vim.fn.glob(glob_path)
+		vim.notify("g:" .. glob_path)
+		vim.notify("|" .. tostring(found ~= "") .. "|")
+
 		if found ~= "" then
 			found_project_root = true
+			local project_file_name = string.gsub(found, ".csproj", "")
+			table.insert(namespace_parts, 1, project_file_name)
+			break
+		end
+		if #path_parts > 0 then
+			table.insert(namespace_parts, 1, table.remove(path_parts))
+		else
 			break
 		end
 	end
@@ -28,6 +42,7 @@ return function(path)
 	if found_project_root then
 		namespace = table.concat(namespace_parts, ".")
 	end
+	vim.notify("ns: " .. namespace)
 
 	return namespace
 end
